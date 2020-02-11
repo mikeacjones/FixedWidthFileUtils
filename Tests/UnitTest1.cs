c810e50a4bce348019795d6664680c4b88ea1481
mjones2219 <mjones@kotrs.com>
2020-02-11 15:09:46 -0500

    Updated tests to use embedded resource.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -2,8 +2,11 @@
 using FixedWidthFileUtils.Serializers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace Tests
 {
@@ -13,15 +16,108 @@ namespace Tests
         [TestMethod]
         public void TestDeserializeWFFile()
         {
-            string testFile = @"C:\temp\pos_AP_20200207211501810.txt";
-            PositivePayFile file = null;
-            using (FileStream fs = new FileStream(testFile, FileMode.Open))
+            PositivePayFile actualFile = FixedWidthSerializer.Deserialize<PositivePayFile>(Properties.Resources.pos_AP_20200207211501810);
+
+            PositivePayFile expectedFile = new PositivePayFile
             {
-                file = FixedWidthSerializer.Deserialize<PositivePayFile>(fs);
-            }
+                Header = new FileHeader
+                {
+                    AccountNumber = 8019000283,
+                    BankID = 164
+                },
+                CheckGroups = new[]
+                {
+                    new CheckGroup
+                    {
+                        Records = new []
+                        {
+                            new CheckRecord
+                            {
+                                CheckSerial = 19983,
+                                AccountNumber = 5555333111,
+                                Amount = 1050.00m,
+                                IssueDate = new DateTime(2020, 02, 03),
+                                Payee = "T & T TESTPAYEE, L.P"
+                            }
+                        },
+                        Trailer = new CheckGroupTrailer
+                        {
+                            RecordCount = 1,
+                            TotalAmount = 1050.00m
+                        }
+                    }
+                }
+            };
+
+
+            Assert.IsTrue(expectedFile.ObjectEquals(actualFile));
         }
     }
 
+    public static class TestExtensions
+    {
+        public static bool ObjectEquals<T>(this T source, T target)
+        {
+            if (source == null && target == null) return true;
+            if (source != null && target == null) return false;
+            if (source == null && target != null) return false;
+
+            var props = source.GetType().GetProperties().Where(p => p.CanRead);
+            foreach (var prop in props)
+            {
+                if (prop.PropertyType.IsEnumerable())
+                {
+                    var leftEnumerable = prop.GetValue(source) as ICollection;
+                    var rightEnumerable = prop.GetValue(target) as ICollection;
+
+                    if (leftEnumerable == null && rightEnumerable == null) continue;
+                    if (leftEnumerable == null && rightEnumerable != null) return false;
+                    if (leftEnumerable != null & rightEnumerable == null) return false;
+                    if (leftEnumerable.Count != rightEnumerable.Count) return false;
+
+                    IEnumerator leftEnumerator = leftEnumerable.GetEnumerator();
+                    IEnumerator rightEnumerator = rightEnumerable.GetEnumerator();
+                    while (leftEnumerator.MoveNext() && rightEnumerator.MoveNext())
+                    {
+                        if (!ObjectEquals(leftEnumerator.Current, rightEnumerator.Current))
+                            return false;
+                    }
+                }
+                else if (prop.PropertyType.IsComplexType())
+                {
+                    var left = prop.GetValue(source);
+                    var right = prop.GetValue(target);
+
+                    if (left == null && right == null) continue;
+                    if ((left == null && right != null) || (left != null && right == null))
+                        return false;
+
+                    if (!ObjectEquals(left, right)) return false;
+                } 
+                else
+                {
+                    var defaultComparerProperty = typeof(EqualityComparer<>)
+                        .MakeGenericType(prop.PropertyType)
+                        .GetProperty("Default")
+                        .GetValue(null, null);
+
+                    var methodInfo = defaultComparerProperty.GetType().GetMethod("Equals", new Type[] { prop.PropertyType, prop.PropertyType });
+                    bool result = (bool)methodInfo.Invoke(defaultComparerProperty, new[] { prop.GetValue(source), prop.GetValue(target) });
+                    if (!result) return false;
+                }
+            }
+            return true;
+        }
+        public static bool IsComplexType(this Type t)
+        {
+            return !t.IsValueType && t != typeof(string);
+        }
+        public static bool IsEnumerable(this Type t)
+        {
+            return typeof(IEnumerable).IsAssignableFrom(t) && t != typeof(string);
+        }
+    }
+    
     #region MODELS
     public class PositivePayFile
     {
@@ -44,9 +140,6 @@ namespace Tests
 
         [FixedField(3, 1)]
         public int AlwaysZero => 0;
-
-        [FixedField(4, 61, ' ')]
-        public string Spacing => string.Empty;
     }
     public class CheckRecord
     {
